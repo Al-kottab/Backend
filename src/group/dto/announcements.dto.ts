b01f427a77411505a37052638fa70005663ddc64
omar fareed <fareedomar159@gmail.com>
2023-01-31 00:44:11 +0200

    create announcement dto

--- src/group/dto/announcements.dto.ts
+++ src/group/dto/announcements.dto.ts
@@ -1,7 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-
+import { IsString, IsNotEmpty } from 'class-validator';
 export class AnnouncementDto {
   @ApiProperty({ description: 'announcement text' })
-  @IsStr
+  @IsString()
+  @IsNotEmpty()
   text: string;
 }
