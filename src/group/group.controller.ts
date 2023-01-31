584d6e3dbf0d0990a9ada0857199fab3ba19f8dd
omar fareed <fareedomar159@gmail.com>
2023-01-31 05:08:22 +0200

    some endpoints documentation

--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -15,31 +15,65 @@ import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AnnouncementDto } from './dto/announcements.dto';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @Controller('groups')
 export class GroupController {
   constructor(private readonly groupService: GroupService) {}
 
+  @ApiOperation({ description: 'create group' })
+  @ApiCreatedResponse({
+    description: 'group created successfully',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'you must be a teacher to create a group',
+  })
   @Post()
   create(@Body() createGroupDto: CreateGroupDto) {
     return this.groupService.create(createGroupDto);
   }
 
+  @ApiOperation({ description: 'get my groups' })
+  @ApiOkResponse({ description: 'groups returned successfully' })
+  @ApiUnauthorizedResponse({
+    description: 'you must login to get your groups',
+  })
   @Get('/mine')
   getMyGroups(userId: string) {
     return this.groupService.getMyGroups(userId);
   }
 
+  @ApiOperation({ description: 'get group by id' })
+  @ApiOkResponse({ description: 'group returned successfully' })
+  @ApiBadRequestResponse({
+    description: 'invalid group id',
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.groupService.findOne(+id);
   }
 
+  @ApiOperation({ description: 'update group info' })
+  @ApiCreatedResponse({ description: 'group updated successfully' })
+  @ApiUnauthorizedResponse({
+    description: 'you must be the group owner to update the group',
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateGroupDto: UpdateGroupDto) {
     return this.groupService.update(+id, updateGroupDto);
   }
 
+  @ApiOperation({ description: 'update group logo' })
+  @ApiCreatedResponse({ description: 'logo updated successfully' })
+  @ApiUnauthorizedResponse({
+    description: 'you must be the group owner to update the logo',
+  })
   @Put(':id/logo')
   @UseInterceptors(FileInterceptor('logo')) // argument name
   uploadLogo(
@@ -50,12 +84,28 @@ export class GroupController {
   }
 
   //TODO: decorator to make sure that user is joining the group
+
+  @ApiOperation({ description: 'get announcements of a group' })
+  @ApiOkResponse({
+    description: 'announcements of a group returned succssfully',
+  })
+  @ApiBadRequestResponse({
+    description: 'invalid group id',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'you must be inside the group to get the group announcements',
+  })
   @Get('/:id/announcements')
   getGroupAnnouncements(@Param('id') groupId: string) {
     return this.groupService.getAnnouncements(groupId);
   }
 
-  @Get('/:id/announcements')
+  @ApiOperation({ description: 'create group announcement' })
+  @ApiCreatedResponse({ description: 'announcement created successfully' })
+  @ApiUnauthorizedResponse({
+    description: 'you must be the group owner create announcement',
+  })
+  @Post('/:id/announcements')
   createGroupAnnouncement(
     @Param(':id') groupId: string,
     @Body() announcementDto: AnnouncementDto,
@@ -63,14 +113,42 @@ export class GroupController {
     return this.groupService.createAnnouncement(groupId, announcementDto);
   }
 
+  @ApiOperation({ description: 'get students of specific group' })
+  @ApiOkResponse({ description: 'students are returned successfully' })
+  @ApiBadRequestResponse({
+    description: 'invalid group id',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'you must be inside a group to see its students',
+  })
   @Get('/:id/students')
   getGroupStudents(@Param(':id') groupId: string) {
     return this.groupService.getGroupStudents(groupId);
   }
+
+  @ApiOperation({
+    description:
+      "get students of specific group that doesn't complete the hefz",
+  })
+  @ApiOkResponse({ description: 'students are returned successfully' })
+  @ApiBadRequestResponse({
+    description: 'invalid group id',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'you must be the group owner to see this list',
+  })
   @Get('/:id/students/status/incomplete')
   getUnhafezStudents(@Param(':id') groupId: string) {
     return this.groupService.getUnhafezStudents(groupId);
   }
+
+  @ApiOperation({ description: 'mark student as hafez' })
+  @ApiCreatedResponse({
+    description: 'student is marked successfully',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'you must be a teacher of the group to mark the student',
+  })
   @Post('/:id/students/:student_id/status/complete')
   markStudentAsHafez(
     @Param(':id') groupId: string,
@@ -78,14 +156,36 @@ export class GroupController {
   ) {
     return this.groupService.markStudentAsHafez(groupId, studentId);
   }
+
+  @ApiOperation({ description: 'ask to join a group' })
+  @ApiCreatedResponse({
+    description: 'request is sent successfully',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'you must be a student to request to join this group',
+  })
+  @ApiBadRequestResponse({ description: 'wrong group id' })
   @Put('/:id/students/me')
   askToJoinGroup(@Param(':id') groupId: string) {
     return this.groupService.askToJoinAGroup(groupId);
   }
+
   @Delete('/:id/students/me')
   leaveGroup(@Param(':id') groupId: string) {
     return this.groupService.leaveGroup(groupId);
   }
+
+  @ApiOperation({ description: 'accept student to join a group' })
+  @ApiCreatedResponse({
+    description: 'student is accepted successfully',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'you must be a group owner to accept this student',
+  })
+  @ApiBadRequestResponse({
+    description: 'wrong group id or there is no request from this user',
+  })
+  @ApiBadRequestResponse({ description: 'wrong group id' })
   @Put('/:id/students/:student_id')
   acceptUserToJoinAGroup(
     @Param(':id') groupId: string,
@@ -93,6 +193,7 @@ export class GroupController {
   ) {
     return this.groupService.acceptStudent(groupId, studentId);
   }
+
   @Delete('/:id/students/:student_id')
   deleteUserFromAGroup(
     @Param(':id') groupId: string,
@@ -100,7 +201,18 @@ export class GroupController {
   ) {
     return this.groupService.deleteStudent(groupId, studentId);
   }
-  @Post('/:id/students/:student_id/badge')
+
+  @ApiOperation({ description: 'give student a badge' })
+  @ApiCreatedResponse({
+    description: 'badge is put to the student successfully',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'you must be the group owner to give a badge to the student',
+  })
+  @ApiBadRequestResponse({
+    description: 'wrong group id or there is no student with this id',
+  })
+  @Put('/:id/students/:student_id/badge')
   giveBadgeToStudent(
     @Param(':id') groupId: string,
     @Param(':student_id') studentId: string,
