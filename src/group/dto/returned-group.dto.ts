e4c06110a6b3c36d042f381347af9568c9d43862
omar fareed <fareedomar159@gmail.com>
2023-01-31 23:59:27 +0200

    adding retuerned-group fields

--- src/group/dto/returned-group.dto.ts
+++ src/group/dto/returned-group.dto.ts
@@ -2,11 +2,17 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class ReturnedGroupDto {
-  @ApiProperty({ description: 'id of the group' })
+  @ApiProperty({ description: 'id of the group', example: '1' })
   @IsString()
   id: string;
 
-  @ApiProperty({ description: 'group name' })
+  @ApiProperty({ description: 'group name', example: 'name' })
   @IsString()
   name: string;
+
+  @ApiProperty({ description: 'created at' })
+  createdAt: Date;
+
+  @ApiProperty({ description: 'group logo path', example: '/path/to/logo' })
+  logo: string;
 }
