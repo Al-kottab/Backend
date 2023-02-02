273700f0fbd45dd17a22567efe800d1e54ece9c8
omar fareed <fareedomar159@gmail.com>
2023-02-02 12:57:33 +0200

    remove PrismaModule from global imports

--- src/utils/test.utils.ts
+++ src/utils/test.utils.ts
@@ -1,7 +1,4 @@
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../prisma/prisma.module';
 
-export const globalImports = [
-  ConfigModule.forRoot({ isGlobal: true }),
-  PrismaModule,
-];
+export const globalImports = [ConfigModule.forRoot({ isGlobal: true })];
