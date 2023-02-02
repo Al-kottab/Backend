044fef67b9f5f3dbade9ae6e9e446e901d5b0684
omar fareed <fareedomar159@gmail.com>
2023-02-02 12:41:30 +0200

    initialize prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { GroupModule } from './group/group.module';
 import { OrganizationModule } from './organization/organization.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [OrganizationModule, AuthModule, UserModule, GroupModule],
+  imports: [OrganizationModule, AuthModule, UserModule, GroupModule, PrismaModule],
   controllers: [],
   providers: [],
 })
