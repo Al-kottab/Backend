75634a91750b6fad1eb3150e77b6e5aee0e6e963
omar fareed <fareedomar159@gmail.com>
2023-02-02 13:18:34 +0200

    create prisma tests

--- src/prisma/prisma.service.spec.ts
+++ src/prisma/prisma.service.spec.ts
@@ -5,13 +5,18 @@ import { PrismaService } from './prisma.service';
 describe('PrismaService', () => {
   let service: PrismaService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [...globalImports],
       providers: [PrismaService],
     }).compile();
-
     service = module.get<PrismaService>(PrismaService);
+  });
+
+  describe('test prisma connection', () => {
+    it('must be connected', async () => {
+      await expect(service.$connect()).resolves.not.toThrowError();
+    });
   });
 
   it('should be defined', () => {
--- src/utils/test.utils.ts
+++ src/utils/test.utils.ts
@@ -1,4 +1,5 @@
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from '../prisma/prisma.module';
 
-export const globalImports = [ConfigModule.forRoot({ isGlobal: true })];
+export const globalImports = [
+  ConfigModule.forRoot({ envFilePath: '../../.env.test', isGlobal: true }),
+];
